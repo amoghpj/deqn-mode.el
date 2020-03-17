7fea6a431fece25f76ca931c29ef494d1d9d3744
Jalihal <amogh.jalihal@gmail.com>
2020-03-16 22:24:29 -0400

    Refactor python export
    Introduce class to interface with model smoothly in plain python  export

--- deqn-python.el
+++ deqn-python.el
@@ -11,8 +11,9 @@
                                   "# This file was generated automatically using deqn.el\n"
                                   "from scipy.integrate import odeint\n"
                                   "import numpy as np\n"
-                                  "import matplotlib.pyplot as plt\n"
-                                  "def mymodel(X, t, pars):\n")
+                                  "import matplotlib.pyplot as plt\n\n"
+                                  "class Model:\n"
+                                  "    def model(self, X, t, pars):\n")
   "Default header for pure python export.
 Can be customized to change the library imports.")
 
@@ -26,54 +27,58 @@ Can be customized to change the library imports.")
     (setq-local counter 0)    
     (dolist (pars deqn-param-vals)
       (setq-local deqn-python/body (concat deqn-python/body
-                                           "    " (nth 0 pars) "= pars[" (number-to-string counter) "]\n"))
+                                           "        " (nth 0 pars) "= pars[" (number-to-string counter) "]\n"))
       (setq-local counter(+ 1 counter)))
     ;; Initialize interpretable variables with argument X
     (setq-local counter 0)
     (dolist (eqn deqn-equations)
       (setq-local deqn-python/body  (concat deqn-python/body
-                                            "    " (substring (nth 0 eqn) 1) "="
+                                            "        " (substring (nth 0 eqn) 1) "="
                                             (concat "X[" (number-to-string counter) "]") "\n"))
       (setq-local counter (+ 1 counter)))
     
     ;; Specify equations
     (dolist (eqn deqn-equations)
       (setq-local deqn-python/body  (concat deqn-python/body
-              "    " (nth 0 eqn) "=" (replace-regexp-in-string (regexp-quote "exp") "np.exp" (nth 1 eqn)) "\n")))
+              "        " (nth 0 eqn) "=" (replace-regexp-in-string (regexp-quote "exp") "np.exp" (nth 1 eqn)) "\n")))
     ;; Make return value list dX containing derivatives
-    (setq-local deqn-python/body (concat deqn-python/body "    dX = ["))
+    (setq-local deqn-python/body (concat deqn-python/body "        dX = ["))
     (dolist (eqn deqn-equations)
       (setq-local deqn-python/body (concat deqn-python/body
                (nth 0 eqn) ", ")) )
     (setq-local deqn-python/body (concat deqn-python/body "]\n"))
-    (setq-local deqn-python/body (concat deqn-python/body "    return dX\n"))
+    (setq-local deqn-python/body (concat deqn-python/body "        return dX\n\n"))
 
     ;; Create dictionaries to hold ics and mapper
-    (setq-local deqn-python/icsdict "icsdict = {\n")
-    (setq-local deqn-python/varmapper "varmapper = {\n")    
+    (setq-local deqn-python/body (concat deqn-python/body 
+                                         "    def __init__(self):\n"
+                                         "        self.tmax = 10\n"
+                                         "        self.nsteps = 100\n"))
+    (setq-local deqn-python/icsdict "        self.icsdict = {\n")
+    (setq-local deqn-python/varmapper "        self.varmapper = {\n")    
     (setq-local counter 0)        
     (dolist (ics deqn-init-conds)
       (setq-local deqn-python/icsdict (concat deqn-python/icsdict
-                                              "    '" (nth 0 ics) "':" (nth 1 ics) ",\n"))
+                                              "            '" (nth 0 ics) "':" (nth 1 ics) ",\n"))
       (setq-local deqn-python/varmapper (concat deqn-python/varmapper
-                                                "    " (number-to-string counter) ":'" (nth 0 ics) "',\n"))
+                                                "            " (number-to-string counter) ":'" (nth 0 ics) "',\n"))
       (setq-local counter (+ 1 counter)))
-    (setq-local deqn-python/varmapper (concat deqn-python/varmapper "}\n"))
-    (setq-local deqn-python/icsdict (concat deqn-python/icsdict "}\n"))    
+    (setq-local deqn-python/varmapper (concat deqn-python/varmapper "        }\n"))
+    (setq-local deqn-python/icsdict (concat deqn-python/icsdict "        }\n"))    
 
     ;; Create dictionaries to hold pars and mapper 
-    (setq-local deqn-python/pardict "pardict = {\n")        
-    (setq-local deqn-python/parmapper "parmapper = {\n")    
+    (setq-local deqn-python/pardict "        self.pardict = {\n")        
+    (setq-local deqn-python/parmapper "        self.parmapper = {\n")    
     (setq-local counter 0)        
     (dolist (ics deqn-param-vals)
       (setq-local deqn-python/pardict (concat deqn-python/pardict
-                                              "    '" (nth 0 ics) "':" (nth 1 ics) ",\n"))
+                                              "            '" (nth 0 ics) "':" (nth 1 ics) ",\n"))
       (setq-local deqn-python/parmapper (concat deqn-python/parmapper
-                                                "    " (number-to-string counter) ":'" (nth 0 ics) "',\n"))
+                                                "            " (number-to-string counter) ":'" (nth 0 ics) "',\n"))
       (setq-local counter (+ 1 counter)))
     
-    (setq-local deqn-python/pardict (concat deqn-python/pardict "}\n"))
-    (setq-local deqn-python/parmapper (concat deqn-python/parmapper "}\n"))    
+    (setq-local deqn-python/pardict (concat deqn-python/pardict "        }\n"))
+    (setq-local deqn-python/parmapper (concat deqn-python/parmapper "        }\n"))    
     
     ;; Add mappers and dicts to body
     (setq-local deqn-python/body (concat deqn-python/body
@@ -85,25 +90,34 @@ Can be customized to change the library imports.")
 
     ;; Loop over mapper items to create lists of ics and pars that can be passed
     ;; to model function
-    (setq-local deqn-python/body (concat deqn-python/body 
-                                         "x0 = []\n"
-                                         "for i in range(len(icsdict.keys())):\n"
-                                         "    x0.append(icsdict[varmapper[i]])\n\n"  
-                                         "pars = []\n"
-                                         "for i in range(len(pardict.keys())):\n"
-                                         "    pars.append(pardict[parmapper[i]])\n"
-                                         ))    
-    ;; (dolist (pars deqn-param-vals)
-    ;;   (setq-local deqn-python/body (concat deqn-python/body
-    ;;                                         (nth 1 pars) ", # " (nth 0 pars) "\n")))
-
-    ;; Write function call
-    (setq-local deqn-python/body (concat deqn-python/body 
-                                         "t = np.linspace(0,10, 100)\n"
-                                         "Y = odeint(mymodel, x0, t, (pars,))\n"
-                                         "for i in range(len(Y[0])):\n"
-                                         "    plt.plot(t, Y[:,i])\n"
-                                         "plt.show()\n"
+    (setq-local deqn-python/body (concat deqn-python/body
+                                         "    def simulate(self):\n"
+                                         "        x0 = []\n"
+                                         "        for i in range(len(self.icsdict.keys())):\n"
+                                         "            x0.append(self.icsdict[self.varmapper[i]])\n\n"  
+                                         "        pars = []\n" 
+                                         "        for i in range(len(self.pardict.keys())):\n"
+                                         "            pars.append(self.pardict[self.parmapper[i]])\n"
+                                         "        t = np.linspace(0,self.tmax, self.nsteps)\n"
+                                         "        Y = odeint(self.model, x0, t, (pars,))\n"
+                                         "        ydict = {self.varmapper[vind]:Y[:,vind] for vind in range(len(self.icsdict))}\n\n"
+                                         "        return(t, ydict)\n\n"
+                                         "    def set_tmax(self, tmax):\n"
+                                         "        self.tmax = tmax\n\n"
+                                         "    def set_nsteps(self, nsteps):\n"
+                                         "        self.nsteps = nsteps\n\n"                                         
+                                         "    def set_parameter(self, pardict):\n"
+                                         "        for k,v in pardict.items():\n"
+                                         "            if k in self.pardict.keys():\n"
+                                         "                self.pardict[k] = v\n\n"
+                                         "    def set_ics(self, icsdict):\n"
+                                         "        for k,v in icsdict.items():\n"
+                                         "            if k in self.icsdict.keys():\n"
+                                         "                self.icsdict[k] = v\n\n"                                         
+                                         "    def plotall(self, t, Y):\n"
+                                         "        for i in range(len(Y[0])):\n"
+                                         "            plt.plot(t, Y[:,i])\n"
+                                         "        plt.show()\n"
                                          ))    
     (setq-local outfilename "*deqn-python-model*")
     (setq deqn-write-string deqn-python/body)
